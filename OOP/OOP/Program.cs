c04fb5b8c7fd9412b5389a75a1f2ecbb531065b1
kioma3 <stefurak.kyrylo@chnu.edu.ua>
2025-10-22 12:46:28 +0300

    added first lab

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -10,7 +10,35 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            //FisrtTask();
+            //task2.Second();
+            //task3.Third();
+            //task4.Fourth();
+            //task5.Fifth();
+            //task6.Sixth();
+            //task7.Seventh();
+            //task8.Eights();
+        }
+
+        public static void FisrtTask()
+        {
+            Console.WriteLine("Введіть день: ");
+
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть місяць: ");
+
+            int number2 = int.Parse(Console.ReadLine());
+
+            if (number1 <= 31 && number2 <= 12)
+            {
+                Console.WriteLine(number1 + "." + number2);
+            }
+            else
+            {
+                Console.WriteLine("Число не дійсне");
+            }
+
         }
     }
 }
